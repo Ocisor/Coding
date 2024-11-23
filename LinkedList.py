fc603c26fa9f8e38e898999f263ef135959ff490
Ocisor <JBamford3141@gmail.com>
2024-11-23 15:22:07 +0000

    Linked List Node class init function

--- LinkedList.py
+++ LinkedList.py
@@ -1,2 +1,7 @@
 #Linked lists are made up of nodes. These contain two dataPoints each. A value and a pointer to the next node
-#The
+
+
+class Node:
+    def __init__(self, data, pointer):
+        self.data = data
+        self.pointer = pointer
