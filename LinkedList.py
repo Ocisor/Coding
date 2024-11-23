ae30a2cf41b1a15d6a7ef07720f938f112a86c96
Ocisor <JBamford3141@gmail.com>
2024-11-23 17:56:29 +0000

    Linked List Progress

--- LinkedList.py
+++ LinkedList.py
@@ -1,7 +1,20 @@
 #Linked lists are made up of nodes. These contain two dataPoints each. A value and a pointer to the next node
-
-
 class Node:
     def __init__(self, data, pointer):
         self.data = data
         self.pointer = pointer
+    
+    def updatePointer(self,newLocation):
+        self.pointer = newLocation
+    
+    def updateData(self,newData):
+        self.data = newData
+
+class LinkedList:
+    def __init__(self, length):
+        self.length = length
+        self.list = [Node("",(i+1)) for i in range(self.length)]
+
+    def appendNode(self):
+        
+
