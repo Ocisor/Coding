cfba057d3fa1cdd0d13e99c42e211548576b42a7
J <JBamford3141@gmail.com>
2024-11-24 07:48:50 +0000

    Progress on Appending

--- LinkedList.py
+++ LinkedList.py
@@ -10,14 +10,21 @@ class Node:
     def updateData(self,newData):
         self.data = newData
 
+    def dis(self):
+        print(f"Data is: {self.data}\nPointer is: {self.pointer}")
+
 class LinkedList:
-    def __init__(self, length, name):
-        self.name = name
+    def __init__(self, length):
         self.length = length
         self.list = [Node("",(i+1)) for i in range(self.length)]
 
     def appendNode(self):
-        pass
+        self.templist = [Node("",i+1) for i in range(self.length + 1)]
+        self.tempist = [Node(self.list)]
+        
         #Maybe create a new instance of the linked list object. But with an increased length of one.
         #For this reason. The actual variable name shouldnt matter, as it will be changed. But instead,
         #pass in the name you want the linked list to have as a parameter.
+
+l1 = LinkedList(3)
+ 
