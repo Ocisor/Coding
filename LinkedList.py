cf24881a9174ca83273ca8aec5b77f814376ee79
J <JBamford3141@gmail.com>
2024-11-24 07:52:27 +0000

    Added comments

--- LinkedList.py
+++ LinkedList.py
@@ -1,20 +1,22 @@
 #Linked lists are made up of nodes. These contain two dataPoints each. A value and a pointer to the next node
-class Node:
+class Node: # Initialising the Node class
     def __init__(self, data, pointer):
+        #Taking in the parameters for the data stored in the node and the location of the next node.
         self.data = data
         self.pointer = pointer
     
-    def updatePointer(self,newLocation):
+    def uPoint(self,newLocation): #uPoint stands for update Pointer
         self.pointer = newLocation
     
-    def updateData(self,newData):
+    def uData(self,newData): # uData stands for update Data
         self.data = newData
 
-    def dis(self):
+    def dis(self): #Displays attributes
         print(f"Data is: {self.data}\nPointer is: {self.pointer}")
 
 class LinkedList:
     def __init__(self, length):
+        #Initialising the linked lit with parameter for length. Creates a list of Node objects.
         self.length = length
         self.list = [Node("",(i+1)) for i in range(self.length)]
 
@@ -27,4 +29,3 @@ class LinkedList:
         #pass in the name you want the linked list to have as a parameter.
 
 l1 = LinkedList(3)
- 
