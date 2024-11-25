b2c524eaa078a22246d4f87991848546b2cfb76f
J <JBamford3141@gmail.com>
2024-11-25 13:54:33 +0000

    Insertion Sort

--- School Coding/InsertionSort.py
+++ School Coding/InsertionSort.py
@@ -1,12 +1,19 @@
 def insertionSort(data):
+    print(data)
     for i in range(1,len(data)): #Iterate through each element. Starting at 1 bcs the first item is "already sorted".
-        temp = data[i] #Sets the piece of data that needs to be sorted to the variable temp.
+        temp = data[i]
+        print(temp) #Sets the piece of data that needs to be sorted to the variable temp.
         checkPos = i-1 #Checking the position before the piece of data being re-inserted. checkPos ---> Check Position
-        while checkPos >= 0 and temp < data[i]: #Checking that the checkPos is still within bounds and if should switch.
+        while checkPos >= 0 and temp < data[checkPos]: #Checking that the checkPos is still within bounds and if should switch.
             data[checkPos + 1] = data[checkPos] # Moving each piece of data forward one.
             checkPos -= 1
-        
+            print("Looped") # Decreasing the position being checked as to loop through all of the already "sorted" points.
+        data[checkPos + 1] = temp # Reinserting the piece of data being sorted.
 
+    print(data) # printing the sorted data        
+        
+unsortedData = [2,5,4,6,7,7,8,5,2,90,1,0,3]
+insertionSort(unsortedData)
 
 #[4,5,3,7]
 #Use index 0 as a key. Check index key + 1.
