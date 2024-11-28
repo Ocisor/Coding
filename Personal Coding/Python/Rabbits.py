bd8d775cca6f72822d692f8f542af3e775b9a766
J <JBamford3141@gmail.com>
2024-11-28 14:57:29 +0000

    Added plotting functionality bcs it disappeared

--- Personal Coding/Python/Rabbits.py
+++ Personal Coding/Python/Rabbits.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
-A, B, C, F = 1, 1, 1, 1
-timeStep = 0.0001
+A, B, C, F = 1.2, 0.5, 0.5, 0.2
+timeStep = 0.01
 STEPS = 1000
 x, y = int(input("Starting X value: ")), int(input("Starting Y value: ") )
 x_values = [0] * STEPS
@@ -12,3 +12,6 @@ for day in range(STEPS):
     y_values[day] = y + (((C*x*y)-(F*x))*timeStep)
     x = x_values[day]
     y = y_values[day]
+
+plt.scatter(x_values,y_values)
+plt.show()
