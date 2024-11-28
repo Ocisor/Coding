0f4a327dfc1c1c7b315b0d50860d2b11985f6eac
J <JBamford3141@gmail.com>
2024-11-28 14:02:24 +0000

    Started Rabbits and Foxes file.

---
+++ Personal Coding/Python/Rabbits.py
@@ -0,0 +1,12 @@
+A, B, C, F = 1, 1, 1, 1
+timeStep = 0.001
+STEPS = 1000
+x, y = input("Starting X value: "), input("Starting Y value: ") 
+x_values = [0] * STEPS
+y_values = [0] * STEPS
+
+for day in range(STEPS):
+    x_values[day] = x + (((A*x)-(B*x*y))*timeStep)
+    y_values[day] = y + (((C*x*y)-(F*x))*timeStep)
+    x = x_values[day]
+    y = y_values[day]
