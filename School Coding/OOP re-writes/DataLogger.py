2a55d32214f80a33ec6024e5902c3fd369c751d0
J <JBamford3141@gmail.com>
2024-12-12 10:39:53 +0000

    Defined month class

--- School Coding/OOP re-writes/DataLogger.py
+++ School Coding/OOP re-writes/DataLogger.py
@@ -4,4 +4,15 @@ class day:
         self._night = tNight
 
     def reDay(self): #   reDay == return day
-        return(self.tDay)
+        return(self._day)
+    
+    def reNight(self):
+        return(self._night)
+    
+class month:
+    def __init__(self, len):
+        self.data = [day(input(f"Day {i} temp: "),input(f"Night {i} temp: ")) for i in range(len)]
+        self.len = len
+    
+    def average(self):
+        pass
