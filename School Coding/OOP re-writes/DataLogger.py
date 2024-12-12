64bbde0a30fac68d233d641e517c6bb252a043a9
J <JBamford3141@gmail.com>
2024-12-12 10:50:50 +0000

    Finished the Average function

--- School Coding/OOP re-writes/DataLogger.py
+++ School Coding/OOP re-writes/DataLogger.py
@@ -3,16 +3,25 @@ class day:
         self._day = tDay
         self._night = tNight
 
-    def reDay(self): #   reDay == return day
-        return(self._day)
-    
-    def reNight(self):
-        return(self._night)
+    def reValue(self, time): 
+        if time == 0:
+            return(self._day)
+        else:
+            return(self._night)
     
 class month:
     def __init__(self, len):
-        self.data = [day(input(f"Day {i} temp: "),input(f"Night {i} temp: ")) for i in range(len)]
+        self.data = [day(int(input(f"Day {i} temp: ")),int(input(f"Night {i} temp: "))) for i in range(len)]
         self.len = len
+        self.avrg = [0,0]
     
-    def average(self):
-        pass
+    def findAvg(self):
+        for i in range(2):
+            for j in range(self.len):
+                self.avrg[i] += self.data[i].reValue(i)
+            self.avrg[i] /= self.len
+        return self.avrg
+    
+June = month(3)
+print(June.findAvg())
+    
