0565d93f975e313f732f4dac55ceb62330d515b6
J <JBamford3141@gmail.com>
2024-12-12 11:01:27 +0000

    Added Min and Max

--- School Coding/OOP re-writes/DataLogger.py
+++ School Coding/OOP re-writes/DataLogger.py
@@ -11,16 +11,33 @@ class day:
     
 class month:
     def __init__(self, len):
-        self.data = [day(int(input(f"Day {i} temp: ")),int(input(f"Night {i} temp: "))) for i in range(len)]
+        self.days = [day(int(input(f"Day {i} temp: ")),int(input(f"Night {i} temp: "))) for i in range(len)]
         self.len = len
         self.avrg = [0,0]
+        self.max = [self.days[0].reValue(0),self.days[0].reValue(1)]
+        self.min = [self.days[0].reValue(0),self.days[0].reValue(1)]
     
     def findAvg(self):
         for i in range(2):
             for j in range(self.len):
-                self.avrg[i] += self.data[i].reValue(i)
+                self.avrg[i] += self.days[j].reValue(i)
             self.avrg[i] /= self.len
         return self.avrg
+    
+    def maxTemp(self):
+        for i in range(2):
+            for j in range(self.len):
+                if self.max[i] < self.days[j].reValue(i):
+                    self.max[i] = self.days[j].reValue(i)
+        return self.max
+    
+    def minTemp(self):
+        for i in range(2):
+            for j in range(self.len):
+                if self.min[i] > self.days[j].reValue(i):
+                    self.min[i] = self.days[j].reValue(i)
+        return self.min
+
     
 June = month(3)
 print(June.findAvg())
