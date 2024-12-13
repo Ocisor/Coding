bd15a0e2cd8a95013c5f545b4bb44e572b18cbf1
J <JBamford3141@gmail.com>
2024-12-13 10:49:38 +0000

    lorbtor

---
+++ Personal Coding/Python/lobster.py
@@ -0,0 +1,22 @@
+import random 
+
+print(random.randint(1,6))
+
+class Player:
+    def __init__(self):
+        self.nets = 1
+        self.cash = 100
+
+    buyNets 
+    
+    def riskyTurn():
+        weather = random.randint(1,6)
+        if weather == 5:
+            weather = "rain"
+        elif weather == 6:
+            weather = "storm"
+        else:
+            weather = "clear"
+        
+
+        
