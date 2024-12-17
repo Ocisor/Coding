899cc0b56d0bb70392f488833d49cdaf8dc3cc03
Ocisor <JBamford3141@gmail.com>
2024-12-17 12:12:56 +0000

    Finished Baby Temperature OOP

--- School Coding/Python Practice/Baby Temperature/Object Oriented Baby Temperature.py
+++ School Coding/Python Practice/Baby Temperature/Object Oriented Baby Temperature.py
@@ -3,6 +3,8 @@ class baby:
         self._name = name
         self._age = age
         self.temperatures = []
+        self.outOfBounds = 0
+
     
     def returnName(self):
         return self._name
@@ -12,16 +14,31 @@ class baby:
         return self._age + 1
     
     def takeTemperature(self, newTemp):
-        while newTemp > 50 or newTemp < 20:
+        while newTemp > 45 or newTemp < 30:
             print("Eroneous input. (or the baby is dead)")
             newTemp = int(input("Please re-input temperature: "))
         self.temperatures.append(newTemp)
         if newTemp < 36:
             print("Temperature too low!")
-            return(1)
+            self.outOfBounds += 1
         elif newTemp > 37.5:
             print("Temperature too high")
-            return(1)
+            self.outOfBounds += 1
         else:
             print("Good temps.")
-            return(0)
+    
+    def extremes(self):
+        max = 30
+        min = 45
+        for temp in self.temperatures:
+            if temp > max:
+                max = temp
+            if temp < min:
+                min = temp
+        print("The max temperature was: ", max)
+        print("The min temperature was: ", min)
+        print(f"The range was {max-min}.")
+
+    def bounds(self):
+        print(f"The baby has been beyond the extremes {self.outOfBounds} many times")
+        self.extremes()
