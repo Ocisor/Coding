455f248e0e5a1457b6362e2e3ec505b239d3e26e
Ocisor <JBamford3141@gmail.com>
2024-12-18 09:44:38 +0000

    changes

--- Personal Coding/FilePathPrep.py
+++ Personal Coding/FilePathPrep.py
@@ -4,8 +4,14 @@ def pathPrep(path):
         print(path)
     path = path.split(input("What is the delimeter: "))
     print(path)
-    path = input("Enter the double slash: ").join(path)
-    print(path[2:])
+    finalPath = []
+    for object in path:
+        if " " in object:
+            object = "\"\"" + object + "\"\""
+        finalPath.append(object)
+
+    finalPath = input("Enter the double slash: ").join(path)
+    print(finalPath[2:])
 
 #C:\Users\Jake\Desktop\Projects\Computing Projects\Python\School\Personal Coding\Websites\TestPHP\process_form.php
 
