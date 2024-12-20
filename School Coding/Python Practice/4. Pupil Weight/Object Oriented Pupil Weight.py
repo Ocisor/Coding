86ba1f4e724688879d4b6ccc22eae0f4d6b5c154
Ocisor <JBamford3141@gmail.com>
2024-12-20 14:31:56 +0000

    Finished Pupil Weight

--- School Coding/Python Practice/4. Pupil Weight/Object Oriented Pupil Weight.py
+++ School Coding/Python Practice/4. Pupil Weight/Object Oriented Pupil Weight.py
@@ -1,5 +1,6 @@
 class pupil():
-    def __init__(self, weight, name):
+    def __init__(self, weight, name, diff):
+        self.diff = diff
         self.weight = weight
         self.name = name
     
@@ -7,9 +8,35 @@ class pupil():
         return self.name
     def returnWeight(self):
         return self.weight
+    def returnDiff(self):
+        return self.diff
+    
+    def updName(self):
+        self.name = input("Current name is invalid. Please input an appropriate one.")
+    def updWeight(self):
+        self.weight = int(input("Current Weight is invalid. Please input an appropriate one."))
+
 
 class students():
     def __init__(self, size):
         self.size = size
-        self.students = [pupil(int(input(f"What is the weight of pupil number {i}: ")), str(input(f"What is the name of pupil number {i}: "))) for i in range(self.size)]
-
+        self.students = [pupil(-1,"",0) for i in range(self.size)]
+    
+    def inputData(self,pupilIndex):
+        while self.students[pupilIndex].returnName() == "":
+            self.students[pupilIndex].updName()
+        while self.students[pupilIndex].returnWeight() < 0 or self.students[pupilIndex].returnWeight() > 150:
+            self.students[pupilIndex].updWeight()
+    
+    def populatePupilRecords(self):
+        for i in range(self.size):
+            self.inputData(i)
+    
+    def calcDiff(self):
+        for i in range(self.size):
+            finalWeight = input("Final Weight")
+            self.students[i].diff = abs(self.students[i].returnName() - finalWeight)
+        
+    def outCome(self, pupilID):
+        if self.students[pupilID].diff > 2.5:
+            print("Change beyond 2.5kg.")
