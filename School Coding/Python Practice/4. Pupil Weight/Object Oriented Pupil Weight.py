414b25bd139986f5a181514ac67e0c80bbcbf919
Ocisor <JBamford3141@gmail.com>
2024-12-20 13:23:52 +0000

    made a pupil class with getters and a class for the list of pupils (class class sounds dumb)

--- School Coding/Python Practice/4. Pupil Weight/Object Oriented Pupil Weight.py
+++ School Coding/Python Practice/4. Pupil Weight/Object Oriented Pupil Weight.py
@@ -1,4 +1,15 @@
 class pupil():
     def __init__(self, weight, name):
         self.weight = weight
-        self.mame = name
+        self.name = name
+    
+    def returnName(self):
+        return self.name
+    def returnWeight(self):
+        return self.weight
+
+class students():
+    def __init__(self, size):
+        self.size = size
+        self.students = [pupil(int(input(f"What is the weight of pupil number {i}: ")), str(input(f"What is the name of pupil number {i}: "))) for i in range(self.size)]
+
