47f5de887d43bd6f958d5dbfb73530f1333fbb02
Ocisor <JBamford3141@gmail.com>
2025-01-04 15:23:20 +0000

    Commented Baby Temp0

--- School Coding/Python Practice/2. Baby Temperature/Baby Temperature.py
+++ School Coding/Python Practice/2. Baby Temperature/Baby Temperature.py
@@ -1,8 +1,9 @@
+#Initialising variables for the program to run
 maxTemp = 0.0
 minTemp = 99.0
 outOfRange = 0
 
-def dataCheck(data):
+def dataCheck(data): #Checks data's ranges
   if data < 36:
     print("Temperature too low!")
     return(1)
@@ -13,10 +14,10 @@ def dataCheck(data):
     print("Good temps.")
     return(0)
 
-for i in range(0,17):
+for i in range(0,17): #Checks 17 values
   dataStore = input("Input data")
-
-  if dataStore > maxTemp:
+  #Checking and setting max and mins
+  if dataStore > maxTemp: 
     maxTemp = dataStore
   if dataStore < minTemp:
     minTemp = dataStore
@@ -25,9 +26,9 @@ for i in range(0,17):
 
 range = maxTemp-minTemp
 
+#Printing summary
 print("Max, min and range of temps are:")
 print(f"{maxTemp}\n{minTemp}\n{range}")
-
 if outOfRange > 2:
   print("The baby's temperature went beyond the acceptable range more than three times.")
 if range > 1:
