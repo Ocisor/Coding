dc5b574e773b199d9a6cee1dc312db4b129a51f8
Ocisor <JBamford3141@gmail.com>
2025-01-04 16:56:46 +0000

    Updates to Parcel

--- School Coding/Python Practice/7. Parcel Weight/Parcel.py
+++ School Coding/Python Practice/7. Parcel Weight/Parcel.py
@@ -1,12 +1,25 @@
 class parcel():
-    def __init__(self, dimensions, weight):
-        self.dimensions = dimensions
-        #dimensions is an array [x, y, z]
-        self.weight = weight
+    def __init__(self):
+        self.dims = [0,0,0]
+        #dims is an array [x, y, z]
+        self.weight = 0
     
-    def checkDims(self): #Check Dimensions
-        for i in range(3):
-            if self.dimensions[i] > 80 or self.dimensions[i] <= 0:
-                print(f"Value no.{i+1} is out of bounds. Please reinput: ")
+    def inputDims(self):
 
-ParcelOne = parcel([x, y, z], weight)
+
+    def checkDims(self): #Dims is short for dimensions
+        self.totalDims = 0
+        for i in range(3):
+            if self.dims[i] > 80 or self.dims[i] <= 0:
+                self.dims[i] = int(input(f"Value no.{i+1} is out of bounds. Please reinput: "))
+            self.totalDims += self.dims[i]
+        if self.totalDims > 200:
+
+
+        
+    
+
+
+
+
+ParcelOne = parcel([x, y, z], weight) #Variables are placeholders as of now for demonstration purposes.
