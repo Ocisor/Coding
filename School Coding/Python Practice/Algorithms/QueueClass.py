52f5d8cfc29224c6a67cf2674d0374c02c5d4c49
Ocisor <JBamford3141@gmail.com>
2025-01-04 15:06:52 +0000

    Commented the queue

--- School Coding/Python Practice/Algorithms/QueueClass.py
+++ School Coding/Python Practice/Algorithms/QueueClass.py
@@ -1,17 +1,17 @@
-#Queues require data to be en-queued and de-queued.
-#Create a queue as a class and pass in data.
-
 class Queue:
+    #Initialising queue length (size) and the data stored within. Along with the pointer.
     def __init__(self, size):
         self.size = size
         self.data = ["" for i in range(self.size)]
         self.pointer = 0
 
+    #Getter for data
     def DisplayQueue(self):
         print(self.data)
 
     def enQueue(self, nData): #nData is newData
         if self.pointer < self.size:
+            #Adds data to the queue.
             self.data[self.pointer] = nData
             self.pointer += 1
             print(f"En-queued data {nData}")
@@ -23,17 +23,10 @@ class Queue:
         if self.pointer != 0: #Checks the queue is not empty.
             print(f"De-queued data {self.data[0]}.")
             self.DisplayQueue()
+            #Dequeues initial element
             for i in range(self.size):
                 if (i+1) < self.size:
                     self.data[i] = self.data[i+1]
             self.pointer -= 1
         else:
             print("Queue is empty.")
-
-q = Queue(3)
-while True:
-    a = input("E or D: ")
-    if a.upper() == "E":
-        q.enQueue(input("What data do you want to queue? "))
-    elif a.upper() == "D":
-        q.deQueue()
