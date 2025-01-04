48424cf70abd9502f7c1c8a8d7e85aa109de4d22
Ocisor <JBamford3141@gmail.com>
2025-01-04 16:13:31 +0000

    Finished/fixed supermarket

--- School Coding/Python Practice/6. Supermarket/Supermarket.py
+++ School Coding/Python Practice/6. Supermarket/Supermarket.py
@@ -1,23 +1,29 @@
-def donationSystem():
-    
-    #Declaring Data Structures
-    charities = []
-    choice = 0
+#Declaring Data Structures and setting up charities
+totals = []
+for i in range(3):
+    totals.append([input(f"Input Charity no.{i+1} "),0])
 
-    for i in range(3):
-        charities.append([input(f"Input Charity no.{i+1} "),0])
+#Function for each user to donate and see the options
+def donationSystem(charities):
+    choice = -2
 
+    #Displaying candidates
     print("\n\nCandidate options are:")
     for i in range(3):
         print(f"{i+1}. {charities[i]}")
-    choice = input("What is your choice?")
-    bill = input("What is your shopping bill?")
+    while choice < 0 or choice > 3:    
+        choice = int(input("As a number 1-3 what is your choice? ")) - 1 #Inputting and validating charity choice.
+    bill = int(input("What is your shopping bill? "))
     donation = bill * 0.01
-
+    print(charities)
     charities[choice][1] += donation
-    print(f"Donated {charities[choice][1]} to {charities[choice][1]}.")
+    print(f"Donated {charities[choice][1]} to {charities[choice][0]}.") #Summary of users action
 
     for i in range(3):
-        print(f"Total for {charities[i][0]} is {charities[choice][1]}.")
+        print(f"Total for {charities[i][0]} is {charities[i][1]}.") #Totals
 
-donationSystem()
+    return charities
+
+
+while True:
+    totals = donationSystem(totals) 
