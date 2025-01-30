0b652b0394aedaa4d72003ceda1b42e4c5d500ab
J <JBamford3141@gmail.com>
2025-01-30 11:11:59 +0000

    First steps

---
+++ School Coding/OOP Booklet/Four in a Row Project.py
@@ -0,0 +1,14 @@
+class Game():
+    def __init__(self):
+        pass
+    def initBoard():
+        pass
+    def createPerson():
+        pass
+    def initPlayers():
+        pass
+
+class Board(Game):
+    def __init__(self, columns, row):
+        self._columns = columns
+        self._row = row
