bff9d90a0039203c603daa9b86245951b2247c61
J <JBamford3141@gmail.com>
2025-03-13 14:24:38 +0000

    Finished :)

--- RSA.py
+++ RSA.py
@@ -3,6 +3,15 @@ e = 23
 n = p*q
 d = [i for i in range(100)]
 
-for i in d:
-    if (i*e) % ((p-1)*(q-1)) == 1 and i != e:
-        print(i)
+#for i in d:
+#    if (i*e) % ((p-1)*(q-1)) == 1 and i != e:
+#        print(i)
+
+def encrypt(value, e, n):
+    return ((value**e) % n)
+
+def decrypt(value, d, n):
+    return ((value**d) % n)
+
+print(encrypt(int(input("To encrypt: ")), e, n))
+print(decrypt(int(input("To decrypt: ")), int(input("Decryption Key: ")), n))
