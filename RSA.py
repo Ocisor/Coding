c53b499a43d0ff1b8d4fc0355ba525f04532a87c
J <JBamford3141@gmail.com>
2025-03-13 14:30:18 +0000

    Sm

--- RSA.py
+++ RSA.py
@@ -1,7 +1,7 @@
-p, q = 5, 7
-e = 23
-n = p*q
-d = [i for i in range(100)]
+p, q = 5, 7 # Two prime numbers
+e = 23 # Coprime to (p-1)(q-1)
+n = p*q # Product of the two primes
+d = [i for i in range(100)] # List of 100 integers
 
 #for i in d:
 #    if (i*e) % ((p-1)*(q-1)) == 1 and i != e:
