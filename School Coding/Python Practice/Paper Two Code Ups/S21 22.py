7d3bbc9647da53a041726d76fece44aca5c315ae
J <JBamford3141@gmail.com>
2025-04-02 16:59:22 +0100

    dua

--- School Coding/Python Practice/Paper Two Code Ups/S21 22.py
+++ School Coding/Python Practice/Paper Two Code Ups/S21 22.py
@@ -1,10 +1,15 @@
 def GetStart(location, FNstring): #location is the number of the word in the string "ANT BEAR SAND" for the word BEAR, location is 2.
     sCount = 0 # Space Count
-    cCount = 0 # Charactar Count
+    cCount = 0 # Character Count
     checked = False
     for char in FNstring:
-        if char == " ":
-            sCount += 1
-        if sCount > location:
-            checked = True
-        
+        if not checked:
+            if char == " ":
+                sCount += 1
+                cCount -= 1
+            if sCount > location:
+                checked = True
+            else:
+                cCount += 1
+    return FNstring[(sCount+cCount)-1]
+print(GetStart(2, "Sand Wich"))
