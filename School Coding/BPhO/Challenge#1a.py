7dfc660996498030ecc27ae9a1be0371e3f81601
Ocisor <JBamford3141@gmail.com>
2025-04-09 13:09:39 +0100

    completed challenge 1a

--- School Coding/BPhO/Challenge#1a.py
+++ School Coding/BPhO/Challenge#1a.py
@@ -5,25 +5,22 @@ def refractiveIndex():
     #Constants
     c = 3 * (10**8)
     #coefficients from https://refractiveindex.info/?shelf=3d&book=glass&page=BK7 under the Dispersion Formula Tab
-    a1 = 1.03961212
-    a2 = 0.231792344
-    a3 = 1.01046945
-    b1 = 0.00600069867
-    b2 = 0.0200179144
-    b3 = 103.560653
-    combinedCoef = (a1/(1-b1))+(a2/(1-b2))+(a3/(1-b3))
+    a = [1.03961212, 0.231792344, 1.01046945]
+    b = [0.00600069867, 0.0200179144, 103.560653]
     nValues = []
     λValues = []
-    for i in range(1486000,1527700,417):
-        n = i/1000000
-        λ = math.sqrt(((n**n)-1)/(combinedCoef))
-        nValues.append(n)
-        λValues.append(λ)
+    for i in range(380, 750, 10):
+        λ = i/1000
+        summation = 0
+        for j in range(3):
+            summation += (a[j]*λ*λ)/((λ**2)-b[j])
+        nValues.append(math.sqrt(1+summation))
+        λValues.append(i)
     return nValues, λValues
 
 n,λ = refractiveIndex()
-
-plt.plot(n,λ) #This plot a second line with these values alongside the first line.
+print(n, "\n", λ)
+plt.plot(λ, n) #This plot a second line with these values alongside the first line.
 plt.ylabel('n')
 plt.xlabel('λ /nm')
 plt.show()
