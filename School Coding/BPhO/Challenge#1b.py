e9882b2ffe413eba07d0725acf08111d39113aef
Ocisor <JBamford3141@gmail.com>
2025-04-17 14:28:00 +0100

    Finished challenge 1b Now implementing CIE colour matching algorithm.

--- School Coding/BPhO/Challenge#1b.py
+++ School Coding/BPhO/Challenge#1b.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import math
+
 def determineColour(f): # f for frequency
     if f < 405:
         colour_str = 'Infra Red'
@@ -19,3 +21,20 @@ def determineColour(f): # f for frequency
     else:
         colour_str = 'Ultra Violet'
 
+freqArray = []
+indexArray = []
+
+for frequency in range(1,1000):
+    freqArray.append(frequency)
+    indexArray.append(math.sqrt(1+math.sqrt(1/(1.731-0.261*((frequency/(10**3))**2)))))
+
+#Implementing the CIE colour matching algorithm based on the CIE 1931 colour model (although they spelt it color because they are american :O )
+def freqToHexCode(f):
+    #First convert the frequency to a wavelength
+
+
+
+plt.plot(freqArray, indexArray) #This plot a second line with these values alongside the first line.
+plt.ylabel('Refractive Index')
+plt.xlabel('frequency/THz')
+plt.show()
