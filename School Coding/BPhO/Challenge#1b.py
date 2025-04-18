27d77ec2ff8c117166043355cf86646bd4ee3425
Ocisor <JBamford3141@gmail.com>
2025-04-18 12:17:27 +0100

    continued

--- School Coding/BPhO/Challenge#1b.py
+++ School Coding/BPhO/Challenge#1b.py
@@ -1,25 +1,6 @@
 import matplotlib.pyplot as plt
 import math
-
-def determineColour(f): # f for frequency
-    if f < 405:
-        colour_str = 'Infra Red'
-    elif (f>=405) and ( f < 480 ):
-        colour_str = 'Red'
-    elif (f>=480) and ( f < 510 ):
-        colour_str = 'Orange'
-    elif (f>=510) and ( f < 530 ):
-        colour_str = 'Yellow'
-    elif (f>=530) and ( f < 600 ):
-        colour_str = 'Green'
-    elif (f>=600) and ( f < 620 ):
-        colour_str = 'Cyan'
-    elif (f>=620) and ( f < 680 ):
-        colour_str = 'Blue'
-    elif (f>=680) and ( f <= 790 ):
-        colour_str = 'Violet'
-    else:
-        colour_str = 'Ultra Violet'
+from math import e
 
 freqArray = []
 indexArray = []
@@ -31,14 +12,28 @@ for frequency in range(1,1000):
 #Implementing the CIE colour matching algorithm based on the CIE 1931 colour model (although they spelt it color because they are american :O )
 def freqToHexCode(f, n):
     c = 3*(10**8)
+
     #First convert the frequency to a wavelength
     wavelength = c / (n * f)
+
+    #Then calculate the hue and saturation of the red-green axis ( X )
     if wavelength < 442:
         x1 = 0.0624 * (wavelength - 442)
     else:
         x1 = 0.0374 * (wavelength - 442)
-    x2
-    x3
+    if wavelength < 599.8:
+        x2 = 0.0264 * (wavelength - 599.8)
+    else:
+        x2 = 0.0323 * (wavelength - 599.8)
+    if wavelength < 501:
+        x3 = 0.0490 * (wavelength - 501.1)
+    else:
+        x3 = 0.0382 * (wavelength - 501.1)
+    X = (0.362*e**((-0.5)*x1**2)) + (1.056*e**((-0.5)*x2**2)) - (0.065*e**((-0.5)*x3**2))
+
+    #Then calculate the luminosity ( Y )
+    if wavelength < 568.8:
+        
 
 
 
