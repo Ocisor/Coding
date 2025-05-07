dff9d5c25b86116ebc564d3f5d663af7cb2b9e7b
Ocisor <JBamford3141@gmail.com>
2025-05-07 17:42:10 +0100

    Coding Plus File sorting

--- School Coding/Emanuel School Blog/nodeProject/app.js
+++ School Coding/Emanuel School Blog/nodeProject/app.js
@@ -4,8 +4,10 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set('view engine', "ejs")
+
 app.get('', (req, res) => {
-    res.send("Hello World!")
+    res.render('base')
 });
 
 app.listen(PORT, ()=> {
