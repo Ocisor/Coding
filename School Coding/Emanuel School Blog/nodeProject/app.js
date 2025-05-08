f9749d5783834757cdcf57fd421063ec7fd0dbb4
J <JBamford3141@gmail.com>
2025-05-08 08:35:23 +0100

    Added 404 error

--- School Coding/Emanuel School Blog/nodeProject/app.js
+++ School Coding/Emanuel School Blog/nodeProject/app.js
@@ -23,3 +23,7 @@ app.use(express.static("public"));
 app.listen(PORT, ()=> {
     console.log(`App listening on port ${PORT}`)
 });
+
+app.use((req, res, next) => {
+    res.status(404).send("<h1>Error 404</h1>Sorry can't find that!")
+});
